ee70486a81700ea6f46e84927cbd8153b65b0cb8
Oleg <konovalov.o.i@mail.ru>
2020-04-26 16:23:33 +0400

    test

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+        
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            })
+        })
+    })
+
+    $('[data-modal=lupa]').on('click', function() {
+        $('.search').fadeToggle('slow')
+    });
+     
+});
